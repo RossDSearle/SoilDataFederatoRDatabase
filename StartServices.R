9539be9fc04c5482c8fb041d2ce488546e93a4c3
RossDSearle <ross.searle@csiro.au>
2019-05-23 15:23:58 +1000

    Same as above

--- StartServices.R
+++ StartServices.R
@@ -1,18 +1,9 @@
 library(plumber)
 library(htmltidy)
 
-#deployDir <-'/srv/plumber/TERNLandscapes/SoilDataFederatoRDatabase'
+deployDir <-'/srv/plumber/TERNLandscapes/SoilDataFederatoRDatabase'
 
-machineName <- as.character(Sys.info()['nodename'])
-if(machineName=='soils-discovery'){
-  deployDir <<- '/srv/plumber/TERNLandscapes/SoilDataFederatoRDatabase'
-  server <- 'http://esoil.io'
-}else{
-  deployDir <<- 'C:/Users/sea084/Dropbox/RossRCode/Git/TERNLandscapes/APIs/SoilDataFederatoRDatabase'
-  server <- 'http://localhost'
-}
-
-
+server <- 'http://esoil.io'
 portNum <- 8076
 
 r <- plumb(paste0(deployDir, "/apiEndPoints_TERNSoilDB.R"))  
@@ -21,9 +12,6 @@ print(r)
 options("plumber.host" = "0.0.0.0")
 options("plumber.apiHost" = "0.0.0.0")
 r$run(host= server, port=portNum, swagger=TRUE)
-
-
-
 
 
 
